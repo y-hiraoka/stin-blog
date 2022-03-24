1fe252175d9294905d9c0199f528935c59d930e5
y-hiraoka <y-hiraoka@murauchi.com>
2022-03-24 19:52:36 +0900

    https に修正

--- src/lib/getFaviconUrl.ts
+++ src/lib/getFaviconUrl.ts
@@ -1,5 +1,5 @@
 export function getFaviconUrl(pageUrl: string, size: 32 | 64 = 64) {
-  return `http://www.google.com/s2/favicons?domain=${encodeURIComponent(
+  return `https://www.google.com/s2/favicons?domain=${encodeURIComponent(
     pageUrl,
   )}&size=${size}`;
 }
