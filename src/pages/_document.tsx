7bc5ca0d84089ba9fa664fec715fb24e76532baa
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2023-01-18 23:27:47 +0900

    apply font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
           <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4010956213409647"
--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -2,4 +2,5 @@ import { globalStyle } from "@vanilla-extract/css";
 
 globalStyle("body", {
   lineHeight: 1.5,
+  fontFamily: ["'Noto Sans JP'", "sans-serif"].join(","),
 });
