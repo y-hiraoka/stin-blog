387595efa4dca8bb74fd57bb8537e1475db3e67b
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2023-01-29 13:54:18 +0900

    ZennArticles から chakra を剥がす

--- src/components/pages/ZennArticles.tsx
+++ src/components/pages/ZennArticles.tsx
@@ -1,10 +1,11 @@
-import { Box, Container, Heading } from "@chakra-ui/react";
 import { FC } from "react";
 import { ArticleList } from "../shared/ArticleList";
 import { ArticlesNavigation } from "../shared/ArticlesNavigation";
 import { Footer } from "../shared/Footer";
 import { Header } from "../shared/Header";
 import { ArticleHeader } from "../../models";
+import { zennArticlesStyles } from "./ZennArticles.css";
+import { PageTitle } from "../shared/PageTitle";
 
 type Props = {
   articles: ArticleHeader[];
@@ -12,18 +13,18 @@ type Props = {
 
 export const ZennArticles: FC<Props> = ({ articles }) => {
   return (
-    <Box>
+    <div>
       <Header />
-      <Container as="main" maxW="container.lg" marginTop="4" marginBottom="16">
-        <Heading as="h1" marginBottom="4" fontSize="2xl">
-          Zenn Articles
-        </Heading>
+      <main className={zennArticlesStyles.container}>
+        <div className={zennArticlesStyles.title}>
+          <PageTitle>Zenn Articles</PageTitle>
+        </div>
         <ArticlesNavigation />
-        <Box marginTop="8">
+        <div className={zennArticlesStyles.articleList}>
           <ArticleList articles={articles} />
-        </Box>
-      </Container>
+        </div>
+      </main>
       <Footer />
-    </Box>
+    </div>
   );
 };
