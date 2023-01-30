dbc2553096f4b5c06625ca090bfedca7f82b9199
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2023-01-30 22:23:10 +0900

    gtag script の位置を変えてみる

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,37 @@
 import "ress";
 import "../styles/global.css";
 import { AppProps } from "next/app";
-import { useGoogleAnalytics } from "../lib/gtag";
+import { GA_TRACKING_ID, useGoogleAnalytics } from "../lib/gtag";
 import { ColorModeProvider } from "../lib/colorMode";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   useGoogleAnalytics();
 
   return (
-    <ColorModeProvider>
-      <Component {...pageProps} />
-    </ColorModeProvider>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <ColorModeProvider>
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
-import { GA_TRACKING_ID } from "../lib/gtag";
 
 export default class MyDocument extends Document {
   render() {
@@ -21,23 +20,6 @@ export default class MyDocument extends Document {
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4010956213409647"
             crossOrigin="anonymous"
-          />
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_TRACKING_ID}', {
-            page_path: window.location.pathname,
-          });
-        `,
-            }}
           />
         </Head>
         <body>
