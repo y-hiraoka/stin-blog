a13e388f812c20d71baec23b417b931f288b6803
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2023-05-27 21:54:51 +0900

    型エラー修正

--- src/components/shared/ArticleTweetCard.tsx
+++ src/components/shared/ArticleTweetCard.tsx
@@ -4,6 +4,6 @@ import { useColorMode } from "../../lib/colorMode";
 import { EmbeddedTweet } from "./EmbeddedTweet";
 
 export const ArticleTweetCard: React.FC<{ url: string }> = ({ url }) => {
-  const { colorMode } = useColorMode();
-  return <EmbeddedTweet url={url} theme={colorMode} lang="ja" />;
+  const { actualColorMode } = useColorMode();
+  return <EmbeddedTweet url={url} theme={actualColorMode} lang="ja" />;
 };
