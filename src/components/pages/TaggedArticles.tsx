e796f58373c209c76d165d9dc54a3548b061b7ec
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2023-11-04 17:56:51 +0900

    タグ記事一覧改修

--- src/components/pages/TaggedArticles.tsx
+++ src/components/pages/TaggedArticles.tsx
@@ -1,23 +1,23 @@
 import { FC } from "react";
 import { ArticleList } from "../shared/ArticleList";
-import { ArticleHeader } from "../../models";
+import { BlogArticleHeader } from "../../models";
 import { TagLink } from "../shared/TagLink";
 import classes from "./TaggedArticles.module.scss";
 import { PageTitle } from "../shared/PageTitle";
 
 type Props = {
   tagName: string;
-  articles: ArticleHeader[];
+  articles: BlogArticleHeader[];
 };
 
 export const TaggedArticles: FC<Props> = ({ tagName, articles }) => {
   return (
-    <main className={classes.container}>
+    <div>
       <div className={classes.titleSection}>
         <PageTitle>Tagged with</PageTitle>
         <TagLink tag={tagName} />
       </div>
       <ArticleList articles={articles} />
-    </main>
+    </div>
   );
 };
