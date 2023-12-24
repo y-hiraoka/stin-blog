608b86759e194b027e63eaec1d7973c90a8b9035
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2023-12-25 01:34:00 +0900

    esm プロジェクトに戻す

---
+++ next-contentlayer.cjs
@@ -0,0 +1,4 @@
+// NOTE: next-contentlayer を CJS で扱うための一時的な踏み台ファイル
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+module.exports = require("next-contentlayer");
--- next.config.js
+++ next.config.js
@@ -1,26 +1,31 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const nextBundleAnalyzer = require("@next/bundle-analyzer");
-const { withContentlayer } = require("next-contentlayer");
+import nextBundleAnalyzer from "@next/bundle-analyzer";
 
 const withBundleAnalyzer = nextBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer(
-  withContentlayer({
-    pageExtensions: ["ts", "tsx", "md", "mdx"],
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "res.cloudinary.com",
-          pathname: "/zenn/image/upload/**",
-        },
-      ],
-    },
-    experimental: {
-      typedRoutes: true,
-    },
-  }),
-);
+const config = async () => {
+  const { withContentlayer } = await import("./next-contentlayer.cjs");
+
+  return withBundleAnalyzer(
+    withContentlayer({
+      pageExtensions: ["ts", "tsx", "md", "mdx"],
+      images: {
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "res.cloudinary.com",
+            pathname: "/zenn/image/upload/**",
+          },
+        ],
+      },
+      experimental: {
+        typedRoutes: true,
+      },
+    }),
+  );
+};
+
+export default config;
