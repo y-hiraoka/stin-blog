ae7a96688da4bf073801f3eaf9d4266863058d58
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2024-08-04 16:49:54 +0900

    error handling

--- src/lib/fetchSiteMetadata.ts
+++ src/lib/fetchSiteMetadata.ts
@@ -10,46 +10,50 @@ export type SiteMetadata = {
 };
 
 export async function fetchSiteMetadata(url: string): Promise<SiteMetadata | null> {
-  const response = await fetch(url, { next: { revalidate: 60 * 60 * 24 * 7 } });
+  try {
+    const response = await fetch(url, { next: { revalidate: 60 * 60 * 24 * 7 } });
 
-  if (!response.ok) {
+    if (!response.ok) {
+      return null;
+    }
+
+    const html = await response.text();
+    const metadata: SiteMetadata = { url };
+    const $ = cheerio.load(html);
+
+    const title = $("title").text().trim();
+    metadata.title = title;
+
+    const metaTags = $("meta");
+
+    for (const meta of metaTags) {
+      const property = meta.attribs.property;
+
+      if (property === "og:site_name") {
+        metadata.site_name = meta.attribs.content || undefined;
+      }
+      if (property === "og:title") {
+        metadata.title = meta.attribs.content || title;
+      }
+      if (property === "og:description") {
+        metadata.description = meta.attribs.content || undefined;
+      }
+      if (property === "og:image") {
+        metadata.image = meta.attribs.content || undefined;
+      }
+      if (property === "og:type") {
+        metadata.type = meta.attribs.content || undefined;
+      }
+
+      const metaName = meta.attribs.name;
+
+      if (metaName === "description") {
+        metadata.description = meta.attribs.content || undefined;
+      }
+    }
+
+    return metadata;
+  } catch (error) {
     return null;
   }
-
-  const html = await response.text();
-  const metadata: SiteMetadata = { url };
-  const $ = cheerio.load(html);
-
-  const title = $("title").text().trim();
-  metadata.title = title;
-
-  const metaTags = $("meta");
-
-  for (const meta of metaTags) {
-    const property = meta.attribs.property;
-
-    if (property === "og:site_name") {
-      metadata.site_name = meta.attribs.content || undefined;
-    }
-    if (property === "og:title") {
-      metadata.title = meta.attribs.content || title;
-    }
-    if (property === "og:description") {
-      metadata.description = meta.attribs.content || undefined;
-    }
-    if (property === "og:image") {
-      metadata.image = meta.attribs.content || undefined;
-    }
-    if (property === "og:type") {
-      metadata.type = meta.attribs.content || undefined;
-    }
-
-    const metaName = meta.attribs.name;
-
-    if (metaName === "description") {
-      metadata.description = meta.attribs.content || undefined;
-    }
-  }
-
-  return metadata;
 }
