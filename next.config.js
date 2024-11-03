09e66ed1425cec4cf179ba16dcaa250cd4be0c91
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2024-11-03 23:57:00 +0900

    contentlayerを直接つかうように修正

--- next.config.js
+++ next.config.js
@@ -1,27 +1,24 @@
 import nextBundleAnalyzer from "@next/bundle-analyzer";
-import { withContentlayer } from "./next-contentlayer.cjs";
 
 const withBundleAnalyzer = nextBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
 });
 
-const config = withBundleAnalyzer(
-  withContentlayer({
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "res.cloudinary.com",
-          pathname: "/zenn/image/upload/**",
-        },
-      ],
-    },
-    experimental: {
-      staleTimes: {
-        dynamic: 30,
+const config = withBundleAnalyzer({
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/zenn/image/upload/**",
       },
+    ],
+  },
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
     },
-  }),
-);
+  },
+});
 
 export default config;
